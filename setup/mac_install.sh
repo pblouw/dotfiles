807a84558cd486f0209ab06e16b77f81df55a739
Peter Blouw <pblouw@gmail.com>
2019-11-04 21:51:16 -0500

    Update Mac setup script

--- setup/mac_install.sh
+++ setup/mac_install.sh
@@ -2,14 +2,9 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew doctor
 brew update
 
-# So we use all of the packages we are about to install
-echo "export PATH='/usr/local/bin:$PATH'\n" >> ~/.bashrc
-source ~/.bashrc
-
-brew install git
+brew install cmake
 brew install zsh
 brew install zsh-syntax-highlighting
-brew install vim 
 brew install tmux
 
 chsh -s /bin/zsh
